985c16b7d398d81a4d5f77145f64f9b66780f120
Ciro Cancello <cirocancello75@gmail.com>
2025-06-28 17:57:55 +0200

    Implementazione di jsp per gestire gli errori e classi javaper l'autenticazione tramite google

---
+++ src/main/java/auth/Secrets.java
@@ -0,0 +1,17 @@
+package auth;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Secrets {
+
+	@SerializedName(value = "client_secret")
+	private String clientSecret;
+
+	public Secrets(String clientSecret) {
+		this.clientSecret = clientSecret;
+	}
+
+	public String getClientSecret() {
+		return this.clientSecret;
+	}
+}
